19d7374a31f0630c40d0197f41edf329ab48111f
Rodri Marchese <rodrimarchese@gmail.com>
2024-02-14 16:17:03 -0300

    Add logout functionality to navbar

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -9,6 +9,7 @@ import { isTeacher } from "@/lib/teacher";
 
 import { SearchInput } from "./search-input";
 import { useAuth } from "@/hooks/useAuth";
+import { signOut } from "next-auth/react";
 
 export const NavbarRoutes = () => {
   const { userId } = useAuth();
@@ -19,6 +20,10 @@ export const NavbarRoutes = () => {
   const isCoursePage = pathname?.includes("/courses");
   const isSearchPage = pathname === "/search";
 
+  const handleLogOut = async () => {
+    await signOut();
+  };
+
   return (
     <>
       {isSearchPage && (
@@ -26,22 +31,31 @@ export const NavbarRoutes = () => {
           <SearchInput />
         </div>
       )}
+
       <div className="flex gap-x-2 ml-auto">
         {isTeacherPage || isCoursePage ? (
           <Link href="/">
-            <Button size="sm" variant="ghost">
-              <LogOut className="h-4 w-4 mr-2" />
-              Salir
-            </Button>
+            <Button size="sm">Ir a modo Agente</Button>
           </Link>
         ) : isTeacher(userId) ? (
           <Link href="/teacher/courses">
-            <Button size="sm" variant="ghost">
-              Modo profesor
-            </Button>
+            <Button size="sm">Ir a modo Admin</Button>
           </Link>
         ) : null}
         {/* TODO: Make a button to sign out? */}
+      </div>
+      {/* button de logout */}
+      <div className="ml-5">
+        <Link href="/">
+          <Button
+            size="sm"
+            variant="ghost"
+            onClick={handleLogOut}
+          >
+            <LogOut className="h-4 w-4 mr-2" />
+            Cerrar sesión
+          </Button>
+        </Link>
       </div>
     </>
   );
