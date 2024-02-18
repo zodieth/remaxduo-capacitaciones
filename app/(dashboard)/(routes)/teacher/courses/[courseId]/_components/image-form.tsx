821009683876d7835bb2962877d3f4e89d0ebe83
Rodri Marchese <rodrimarchese@gmail.com>
2024-02-18 00:12:48 -0300

    testing log in production

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/image-form.tsx
@@ -46,6 +46,8 @@ export const ImageForm = ({
     }
   };
 
+  console.log("Initial data: ", initialData);
+
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-between">
